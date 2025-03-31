2999894bcb7002cd63fdfdaa777444ee539e8ba4
azsx92 <azsx92@gmail.com>
2025-03-31 15:29:40 +0900

    feat: sychronized end

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -13,6 +13,9 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
    // @Transactional
+    // synchronized 단일 서버 안에서는 레이스 컨디션 문제가 발생하지 않지만
+    // 최근 에는 서버가 두개 이상 인 경우가 대부분 이므로 레이스 컨디션 문제가 발생 하기가 쉽다.
+    // 그래서 다음에는 mysql 레이스 컨디션을 방지하는 방법을 알아 보도록 하자.
     public synchronized void decrease(Long id, Long quantity) {
         // Stock 조회
         // 재고를 감소한 뒤
