f0b10b6b3007c9e081354a904831f27f8d26229c
azsx92 <azsx92@gmail.com>
2025-03-31 20:12:03 +0900

    feat: nmaedLock test end

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -12,11 +13,8 @@ public class StockService {
     public StockService(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
-   // @Transactional
-    // synchronized 단일 서버 안에서는 레이스 컨디션 문제가 발생하지 않지만
-    // 최근 에는 서버가 두개 이상 인 경우가 대부분 이므로 레이스 컨디션 문제가 발생 하기가 쉽다.
-    // 그래서 다음에는 mysql 레이스 컨디션을 방지하는 방법을 알아 보도록 하자.
-    public synchronized void decrease(Long id, Long quantity) {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public  void decrease(Long id, Long quantity) {
         // Stock 조회
         // 재고를 감소한 뒤
         // 갱신된 값을 저장하도록 하겠습니다.
